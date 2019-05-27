c0658eef15f0b785195d1a525d44b7acc2296819
Ali Gholami <aligholamiit@gmail.com>
2019-05-27 19:24:10 +0430

    Displaying variablesstored in controller using viewbag

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,7 +10,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Here goes the description of this comic book!</p>";
+            ViewBag.Artists = new string[]
+                    {
+                "Do: The First",
+                "Re: The Second",
+                "Mi: The Third",
+                "Fa: The Fourth"
+                    };
+
             return View();
             
         }
